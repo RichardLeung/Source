9518c302c95a0143e74a9a1936b426093d3a04ff
RichardLeung <79985865@qq.com>
2024-06-10 18:57:53 +0800

    【新增】血条变化和伤害计算方法

---
+++ SimpleRPG/Abilities/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ExecCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+#include "SimpleRPG/Abilities/RPGAttributeSet.h"
+
+struct RPGDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(DEF);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(ATK);
+	
+	RPGDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(URPGAttributeSet, DEF, Target, false)
+		DEFINE_ATTRIBUTE_CAPTUREDEF(URPGAttributeSet, ATK, Source, true)
+	}
+};
+
+static const RPGDamageStatics& DamageStatics()
+{
+	static RPGDamageStatics DmgStatics;
+	return DmgStatics;
+}
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	RelevantAttributesToCapture.Add(DamageStatics().ATKDef);
+	RelevantAttributesToCapture.Add(DamageStatics().DEFDef);
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+                                              FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	const AActor* SourceActor = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	const AActor* TargetActor = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float ATK = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ATKDef, EvaluationParameters, ATK);
+	ATK = FMath::Max<float>(0.f, ATK);
+	++ATK;
+	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ATKProperty, EGameplayModOp::Additive, ATK);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
+	
+}
