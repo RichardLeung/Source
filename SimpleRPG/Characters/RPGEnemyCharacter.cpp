7c9a843e6edc6372bf83587bb89e6dd4e7c56c11
RichardLeung <79985865@qq.com>
2024-06-30 18:14:06 +0800

    【新增】敌人的动画系统

--- SimpleRPG/Characters/RPGEnemyCharacter.cpp
+++ SimpleRPG/Characters/RPGEnemyCharacter.cpp
@@ -96,41 +96,6 @@ void ARPGEnemyCharacter::PossessedBy(AController* NewController)
 
 void ARPGEnemyCharacter::DirectionalHit(const FVector& ImpactPoint)
 {
-	// // 按照攻击部位播放不同的动画
-	// FVector Forward = GetActorForwardVector();
-	// const FVector ImpactLowered(ImpactPoint.X, ImpactPoint.Y, GetActorLocation().Z);
-	// FVector ToHit = (ImpactLowered - GetActorLocation()).GetSafeNormal();
-	//
-	// // Forward * ToHit = |Forward||ToHit| * cos(theta)
-	// // |Forward| = 1, |ToHit| = 1, So Forward * ToHit = cos(theta)
-	// const double CosTheta = FVector::DotProduct(ToHit, Forward);
-	//
-	// // Take the inverse cosine (arc - cosine) of cos(theta) to get theta
-	// double Theta = FMath::Acos(CosTheta);
-	//
-	// // convert from radians to degree
-	// Theta = FMath::RadiansToDegrees(Theta);
-	//
-	// // If CrossProduct points down, Theta is negative
-	// const FVector CrossProduct = FVector::CrossProduct(Forward, ToHit);
-	// if(CrossProduct.Z < 0)
-	// {
-	// 	Theta = -Theta;
-	// }
-	// FName SectionName("FromBack");
-	// if(Theta >= -45.f && Theta < 45.f)
-	// {
-	// 	SectionName = FName("FromFront");
-	// }
-	// else if(Theta >= 45.f && Theta < 135.f)
-	// {
-	// 	SectionName = FName("FromRight");
-	// }
-	// else if(Theta >= -135.f && Theta < -45.f)
-	// {
-	// 	SectionName = FName("FromLeft");
-	// }
-
 	//按照角色站位播放不同的动画
 	FVector EnemyLocation = GetActorLocation();
 	FVector PlayerLocation = PlayerCharacter->GetActorLocation();
